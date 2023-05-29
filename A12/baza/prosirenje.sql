d29b73763ef86470191d67659f0fd1a155d5fe89
Sara Spasojević <98286680+cajjaaa@users.noreply.github.com>
2023-05-29 09:28:36 +0200

    Update prosirenje.sql

--- A12/baza/prosirenje.sql
+++ A12/baza/prosirenje.sql
@@ -1,11 +1,11 @@
-﻿CREATE TABLE igrac (
+CREATE TABLE igrac (
     Ime VARCHAR(50),
     Prezime VARCHAR(50),
+    NazivDrzave VARCHAR(100),
     DatumRodjenja DATE,
     DatumPocetka DATE,
     DatumZavrsetka DATE,
 	KlubID INT REFERENCES Klub(KlubID),
 	CHECK(DatumPocetka<=DatumZavrsetka),
 	PRIMARY KEY(Ime,Prezime,Datumrodjenja)
-	
 );
